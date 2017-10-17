0689cc75eda2607a01ac468a7639de1653446bbb
Kent <kenjishin@gmail.com>
2017-10-17 23:52:58 +1100

    06 Events and ReactJS done

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,6 +2,15 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
+
+  onMakeOlder(){
+    this.age += 3;
+    console.log(this.age);
+  }
   render(){
     let content = "";
     if(true){
@@ -11,18 +20,10 @@ export class Home extends React.Component{
     console.log(this.props);
     return (
       <div>
-      <p>In a new Component!</p>
-      <p>{text}</p>
-      <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-      <p>User Object => Name: {this.props.user.name}</p>
-      <div>
-      <h4>Hoobies</h4>
-      <ul>
-      {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-      </ul>
-      </div>
-      <hr/>
-      {this.props.children}
+          <p>In a new Component!</p>
+          <p>Your name is {this.props.name}, your age is {this.age}</p>
+          <hr/>
+          <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
       </div>
     );
   }
@@ -31,6 +32,4 @@ export class Home extends React.Component{
 Home.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
-  user: PropTypes.object,
-  children: PropTypes.element.isRequired
 };
