05132005e47bef029c15f50f9002a652d61b5742
Kent <kenjishin@gmail.com>
2017-10-18 00:53:46 +1100

    10 Communicating between parent and child done

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -34,6 +34,8 @@ export class Home extends React.Component{
           <p>Status: {this.state.status}</p>
           <hr/>
           <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+          <hr/>
+          <button onClick={() => this.props.greet()} className="btn btn-default">Greet</button>
       </div>
     );
   }
@@ -42,4 +44,5 @@ export class Home extends React.Component{
 Home.propTypes = {
   name: PropTypes.string,
   initialAge: PropTypes.number,
+  greet: PropTypes.func
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,6 +6,9 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class Foo extends React.Component {
+    onGreet(){
+        alert("hello!");
+    }
   render(){
     return (
       <div className="container">
@@ -16,7 +19,7 @@ class Foo extends React.Component {
         </div>
         <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-                <Home name={"Max"} initialAge={27} />
+                <Home name={"Max"} initialAge={27} greet={this.onGreet}/>
             </div>
         </div>
       </div>
