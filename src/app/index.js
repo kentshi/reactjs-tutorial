89864cce97e62d76456a1ea644a69959cce72882
Kent <kenjishin@gmail.com>
2017-10-17 23:41:41 +1100

    05 Passing Data with Props

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,10 @@ import { Home } from "./components/Home";
 
 class Foo extends React.Component {
   render(){
+      var user = {
+          name: "Anna",
+          hobbies: ["Sports", "Reading"]
+      };
     return (
       <div className="container">
         <div className="row">
@@ -16,7 +20,9 @@ class Foo extends React.Component {
         </div>
         <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-                <Home/>
+                <Home name={"Max"} age={27} user={user}>
+                    <p>This is a paragraph</p>
+                </Home>
             </div>
         </div>
       </div>
